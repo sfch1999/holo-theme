9d274abfe9f7410c212fa11f8d06923b16430ef3
sfch1999 <sor100100@yahoo.com>
2025-02-12 16:46:13 -0500

    major change

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ const IndexPage = () => (
   <div className="h-auto w-screen">
     <Layout>
       <Seo
-        title="Holo - Gatsby Theme"
-        description="Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS."
+        title="ABI AI: Accelerate the Development of AI Applications"
+        description="ABI AI is an AI-as-a-Service (AIaaS) provider specializing in delivering custom AI solutions for enterprises. It helps businesses leverage artificial intelligence to optimize operations, enhance decision-making, and drive innovation. ABI AI offers expertise in machine learning, natural language processing (NLP), computer vision, and predictive analytics, enabling companies to integrate AI seamlessly into their workflows. Whether automating processes, improving customer experiences, or extracting insights from data, ABI AI tailors its solutions to meet the unique needs of each enterprise."
       ></Seo>
       <Header></Header>
       <FeatureSection></FeatureSection>
