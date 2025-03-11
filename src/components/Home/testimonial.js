fe0b22408b4ab6c1d3a16b5592716058cb49471c
sfch1999 <sor100100@yahoo.com>
2025-03-11 18:29:37 -0400

    Commit

--- src/components/Home/testimonial.js
+++ src/components/Home/testimonial.js
@@ -8,15 +8,12 @@ const Testimonial = () => {
         <div className="w-full h-full  flex flex-row justify-between align-middle bg-[url('../images/holographic-background.webp')] p-12 rounded-lg xxs:flex-col xs:flex-col sm:flex-row xxs:p-5 lg:p-12">
           <div className="w-3/4 xxs:w-full xs:w-full backdrop-blur-sm bg-white/30 p-10 rounded-lg lg:p-10">
             <div className="text-black font-semibold text-xl xxs:text-s xxs:font-normal sm:text-xl font-notoserif">
-              "Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book."
+              "This groundbreaking work by the authors is revolutionizing the intersection of AI and financial risk management. By applying deep reinforcement learning to gamma and vega hedging, they have set a new standard for AI-driven trading strategies. Their innovative approach not only enhances risk control but also paves the way for the future of autonomous financial decision-making. A true milestone in AI-powered finance."
             </div>
             <div className="mt-10">
-              <h3 className="text-black text-2xl font-smibold">Lorem Ipsum</h3>
+              <h3 className="text-black text-2xl font-smibold">Journal of Banking and Finance</h3>
               <p className="text-black text-sm font-semibold opacity-50">
-                Google review
+                Elsevier
               </p>
             </div>
           </div>
