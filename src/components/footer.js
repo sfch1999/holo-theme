dccb18cbfd7b9f8d9ac84e15ff879dc8c7565afe
sfch1999 <sor100100@yahoo.com>
2025-03-11 18:50:10 -0400

    Commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import { Link } from "gatsby";
 import React from "react";
-import logo from "../images/3D-liquid-abstract-5.webp";
+import logo from "../images/removed-Holographic.png";
 
 const Footer = () => {
   return (
