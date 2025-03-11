b0bba4a1e1adb8f074ab6599f005f8b87c0f1767
sfch1999 <sor100100@yahoo.com>
2025-03-11 15:53:21 -0400

    check

--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -114,28 +114,15 @@ const FeatureSection = () => {
                 <p className="text-black text-2xl">MLOps & Automation</p>
                 <p className="text-black opacity-50 text-sm">Kubeflow & MLflow</p>
               </div>
-              <div className="mt-5">
-                <p className="text-black text-2xl">Data Visualization</p>
-                <p className="text-black opacity-50 text-sm">Plotly & Dash</p>
-              </div>
             </div>
           </div>
 
           <div className="w-4/6 bg-dp p-8 rounded-xl m-5 xxs:w-full xs:w-full sm:w-4/6">
             <h2 className="text-white text-4xl">
-              Start building amazing web experiences
+            Empower Your Business with Cutting-Edge AI
             </h2>
             <p className="mt-10 text-white opacity-70">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Harness the power of artificial intelligence to automate workflows, analyze vast data sets, and enhance decision-making. ABI AI provides custom AI solutions designed to improve efficiency, drive innovation, and give enterprises a competitive edge. Whether you need predictive analytics, natural language processing, or AI-driven automation, our technology seamlessly integrates into your business to unlock new opportunities.
             </p>
           </div>
         </div>
